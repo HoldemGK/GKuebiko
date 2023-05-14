e4f9672787a2182667549baf26403b9ab83d7cc4
HoldemGK <atos.holdemgk@gmail.com>
2023-05-14 20:34:47 +0200

    up creds

--- creds.py
+++ creds.py
@@ -1,8 +1,10 @@
+import os
+
 # You're Twitch Token 
-TWITCH_TOKEN = ""
+TWITCH_TOKEN = os.environ.get('TOKEN')
 # Your TWITCH Channel Name
-TWITCH_CHANNEL = ""
+TWITCH_CHANNEL = os.environ.get('BOT_NAME')
 # Your OpenAI API Key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 # Your Google Cloud JSON Path
-GOOGLE_JSON_PATH = ""
+GOOGLE_JSON_PATH = os.environ.get('GOOGLE_JSON_PATH')
